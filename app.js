eb5bdf861e26d1fd7b2fa8e4d2a57d657b363a4e
Hassan Shaikh <shaikh.hassan01@outlook.com>
2024-05-07 11:51:02 -0400

    created user and project questions

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require("inquirer");
+const { default: Choices } = require("inquirer/lib/objects/choices");
 // const fs = require("fs");
 // const generatePage = require('./src/page-template.js');
 
@@ -10,11 +11,79 @@ const inquirer = require("inquirer");
 //   console.log("Portfolio complete! Check out index.html to see the output!");
 // });
 
-inquirer.prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    }    
-])
-.then(answers => console.log(answers));
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "Enter your GitHub Username",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide some information about yourself:",
+    },
+  ]);
+};
+
+const promptProject = () => {
+  console.log(`
+=================
+Add a New Project
+=================
+`);
+
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "name",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "ConfirmAddProject",
+      message: "Would you like to enter another project",
+      default: false,
+    },
+  ]);
+};
+promptUser()
+  .then((answers) => console.log(answers))
+  .then(promptProject)
+  .then((projectAnswers) => console.log(projectAnswers));
